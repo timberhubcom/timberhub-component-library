9e93a9ffbbbbc8573920e80fb62ba395b00d0cb3
ionpetro <ionpetro@timberhub.com>
2022-09-22 12:06:17 +0200

    save

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,7 +6,7 @@ export interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
-  return <button className={styles.button}>{props.label}</button>;
+  return <button className={`${styles.button} ${styles.test}`}>{props.label}</button>;
 };
 
 export default Button;
