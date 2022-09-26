96811a0a72a24a170b34a129f15032ac9737fec6
ionpetro <ionpetro@timberhub.com>
2022-09-26 17:13:42 +0200

    TextArea

---
+++ src/components/TextArea/TextArea.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { ComponentStory } from '@storybook/react';
+import TextArea from './TextArea';
+
+export default {
+  title: 'Components/TextArea',
+  component: TextArea,
+};
+
+const Template: ComponentStory<typeof TextArea> = (args) => <TextArea {...args} />;
+
+export const Base = Template.bind({});
+Base.args = {
+  label: 'label',
+  placeholder: 'placeholder',
+};
+
+export const Required = Template.bind({});
+Required.args = {
+  label: 'label',
+  required: true,
+  placeholder: 'placeholder',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  label: 'label',
+  disabled: true,
+  placeholder: 'placeholder',
+};
+
+export const Description = Template.bind({});
+Description.args = {
+  label: 'label',
+  description: 'This text describes the input',
+  placeholder: 'placeholder',
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  label: 'label',
+  error: 'There was an error',
+  placeholder: 'placeholder',
+};
