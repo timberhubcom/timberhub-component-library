3b20de6da5c74f4796031c4bd370a9c643c00996
ionpetro <ionpetro@timberhub.com>
2022-09-26 14:40:09 +0200

    Storybook theming

---
+++ .storybook/TimberhubTheme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Timberhub storybook',
+  brandImage: 'https://www.timberhub.com/logo.svg',
+  brandTarget: '_self',
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import TimberhubTheme from './TimberhubTheme';
+
+addons.setConfig({
+  theme: TimberhubTheme,
+});
