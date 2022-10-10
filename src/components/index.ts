60d1c2619ef9144c1417c80be0bc745b613539a8
ionpetro <ionpetro@timberhub.com>
2022-10-10 17:48:06 +0200

    1.1.6

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,3 +8,4 @@ export { default as Switch } from './Switch';
 export { default as Table } from './Table';
 export { default as TextArea } from './TextArea';
 export { default as TextField } from './TextField';
+export { default as SelectItem } from './SelectItem';
