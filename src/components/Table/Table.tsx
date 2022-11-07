a3c20dc518c35ca4600cd8ef519984980fad6c64
dimpagk <97886407+dimpagk@users.noreply.github.com>
2022-11-07 18:56:31 +0100

    Fixes bug with structure of table component

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -30,28 +30,30 @@ const Table: React.FC<TableProps> = ({
       </thead>
       <tbody>
         {rows.length > 0 ? (
-          <tr>
+          <>
             {rows.map((row, position) => (
-              <td
-                className={`${styles['cols']} ${styles['tableRow']} ${clickableRow && styles['tableRowClickable']}`}
-                key={position}
-                onClick={() => handleRowClick(row)}
-                data-testid={`row-${position}`}
-              >
-                {structure.header.map((column, index) => (
-                  <div
-                    className={`${styles['tableColumn']} ${styles[`col-${column.width}`]} ${
-                      column.show_on_hover && styles['showOnHover']
-                    }`}
-                    data-key={column.name}
-                    key={index}
-                  >
-                    {row[column.name] && row[column.name]}
-                  </div>
-                ))}
-              </td>
+              <tr>
+                <td
+                  className={`${styles['cols']} ${styles['tableRow']} ${clickableRow && styles['tableRowClickable']}`}
+                  key={position}
+                  onClick={() => handleRowClick(row)}
+                  data-testid={`row-${position}`}
+                >
+                  {structure.header.map((column, index) => (
+                    <div
+                      className={`${styles['tableColumn']} ${styles[`col-${column.width}`]} ${
+                        column.show_on_hover && styles['showOnHover']
+                      }`}
+                      data-key={column.name}
+                      key={index}
+                    >
+                      {row[column.name] && row[column.name]}
+                    </div>
+                  ))}
+                </td>
+              </tr>
             ))}
-          </tr>
+          </>
         ) : (
           <tr>
             <div className={styles['tableEmpty']} data-testid={'empty-table'}>
