e608b48aaefeea732ea3026d4d8cbf9db3d2e8e0
ionpetro <ionpetro@timberhub.com>
2023-04-13 14:25:07 +0300

    Change loader

--- src/components/Icons/Loader.tsx
+++ src/components/Icons/Loader.tsx
@@ -2,13 +2,7 @@ import React from 'react';
 
 const Loader = ({ color }: { color: string }) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="200px"
-      height="200px"
-      viewBox="0 0 100 100"
-      preserveAspectRatio="xMidYMid"
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
       <circle
         cx="50"
         cy="50"
