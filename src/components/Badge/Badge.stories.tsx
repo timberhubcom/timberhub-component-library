669488cb585140acc4bf20218b9f7d7c5b4b255e
Benjamin Oni <onibenjo@gmail.com>
2023-05-01 20:09:30 -0500

    BP-323 badge

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -14,4 +14,29 @@ Green.args = {};
 
 export const Red = Template.bind({});
 Red.args = {
+  color: 'red',
 };
+export const Orange = Template.bind({});
+Orange.args = {
+  color: 'orange',
+};
+export const Blue = Template.bind({});
+Blue.args = {
+  color: 'blue',
+};
+
+export const WithAvatar = () => (
+  <>
+    <div className="">
+      <Badge color="red" content={5} size="sm">
+        <img src="https://i.pravatar.cc/300?u=a042581f4e29026707d" alt="" width={50} height={50} />
+      </Badge>
+    </div>
+    <br />
+    <div className="">
+      <Badge color="red" content={5} placement="bottom-right" size="sm">
+        <img src="https://i.pravatar.cc/300?u=a042581f4e29026707e" alt="" width={50} height={50} />
+      </Badge>
+    </div>
+  </>
+);
