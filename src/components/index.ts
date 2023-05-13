a5f1ad90c7cb8c60cf03ba2140ed1d6c7bf01e94
Benjamin Oni <onibenjo@gmail.com>
2023-05-13 12:06:21 +0100

    BP-342 UI lib update

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,16 @@
+export { default as Alert } from './Alert';
 export { default as Button } from './Button';
 export { default as Checkbox } from './Checkbox';
 export { default as CheckboxGroup } from './CheckboxGroup';
 export { default as Chip } from './Chip';
+export { default as ItemCard } from './ItemCard';
+export { default as Pagination } from './Pagination';
 export { default as Radio } from './Radio';
 export { default as RadioGroup } from './RadioGroup';
 export { default as SelectItem } from './SelectItem';
 export { default as SelectInput } from './SelectInput';
 export { default as Switch } from './Switch';
 export { default as Table } from './Table';
+export { default as Tabs } from './Tabs';
 export { default as TextArea } from './TextArea';
 export { default as TextField } from './TextField';
