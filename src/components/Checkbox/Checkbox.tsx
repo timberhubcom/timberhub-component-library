2d228e031f5c55d02af799cd6a682a9ed7894beb
Oni Benjamin <onibenjo@gmail.com>
2023-05-16 09:30:01 +0100

    BP-352-remove-value-from-checkbox-option-prop (#15)
    * BP-352 checkbox type
    
    * BP-352 Checkbox/Button type fix

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -11,7 +11,7 @@ type CheckboxProps = {
   name?: string;
   label?: string;
   labelClass?: string;
-  option?: CheckboxOptionType;
+  option?:CheckboxOptionType & {value?: string};
   description?: string;
   defaultValue?: boolean;
   value?: boolean;
