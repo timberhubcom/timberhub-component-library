ad8c6ec8d3cdbd8f327850511bad15b667272471
Benjamin Oni <onibenjo@gmail.com>
2023-06-21 15:08:46 +0100

    TH-22 chip color & table loader

---
+++ playground/src/App.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import { Button } from '@timberhubcom/timberhub-component-library';
+
+import '@timberhubcom/timberhub-component-library/dist/cjs/index.css';
+
+const App = ()  => {
+
+  return (
+      <div>
+      <div>
+
+      <Button>
+        Boy here
+        </Button>
+      </div>
+      </div>
+    );
+}
+
+export default App;
