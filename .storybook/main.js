0a76f921cdf786458a25d09b2c756d93336724b4
Oni Benjamin <onibenjo@gmail.com>
2023-07-19 23:18:23 +0100

    Revert "fix: rollup build ts absolute import path (#38)"
    This reverts commit 8dd0b32f20f7362eec217cb43c3c63e25735ada7.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-scss', '@storybook/addon-mdx-gfm'],
@@ -11,11 +9,4 @@ module.exports = {
     autodocs: true
   },
   staticDirs: ['../src/assets'],
-  webpackFinal: async (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'src': path.resolve(__dirname, '../src/'),
-    };
-    return config;
-  },
 };
--- rollup.config.js
+++ rollup.config.js
@@ -53,13 +53,7 @@ export default [
   {
     input: 'dist/esm/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [
-      dts({
-        compilerOptions: {
-          baseUrl: './',
-        },
-      }),
-    ],
+    plugins: [dts()],
     external: [/\.(css|scss)$/],
   },
 ];
