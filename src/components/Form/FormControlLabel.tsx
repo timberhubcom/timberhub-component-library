9c3d6828af42040c1ffb6d7732354483de0ba1c9
Oni Benjamin <onibenjo@gmail.com>
2023-08-08 12:25:57 +0100

    style: form control label spacing (#55)

--- src/components/Form/FormControlLabel.tsx
+++ src/components/Form/FormControlLabel.tsx
@@ -1,5 +1,6 @@
 import MuiFormControlLabel, { FormControlLabelProps as MuiFormControlLabelProps } from '@mui/material/FormControlLabel';
 import { TypographyVariantOverrides } from '../../types/typography.types';
+import { styled } from '@mui/material/styles';
 
 declare module '@mui/material/Typography' {
   export interface TypographyPropsVariantOverrides extends TypographyVariantOverrides {}
@@ -7,6 +8,11 @@ declare module '@mui/material/Typography' {
 
 export interface FormControlLabelProps extends MuiFormControlLabelProps {}
 
-const FormControlLabel = MuiFormControlLabel as React.FC<FormControlLabelProps>;
+const StyledControlLabel = styled(MuiFormControlLabel)`
+  gap: 8px;
+  margin-left: 0px;
+`;
+
+const FormControlLabel = StyledControlLabel as React.FC<FormControlLabelProps>;
 
 export { FormControlLabel };
