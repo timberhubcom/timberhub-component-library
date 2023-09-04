8f77fecb48c2b9f81c6bf6e7158302a40dfe458d
Benjamin Oni <onibenjo@gmail.com>
2023-09-04 08:37:52 +0100

    fix: data item multiline

--- src/components/DataItem/DataItem.tsx
+++ src/components/DataItem/DataItem.tsx
@@ -5,6 +5,7 @@ import { cx, css } from '@emotion/css';
 import { tokens } from '../../theme/tokens';
 
 import { Typography } from '../Typography';
+import styled from '@emotion/styled';
 
 export type DataItemsProps = {
   icon: string;
@@ -13,18 +14,25 @@ export type DataItemsProps = {
   className?: string;
 };
 
+const DataItemWrapper = styled(Paper)`
+  display: grid;
+  grid-template-columns: auto 1fr;
+`;
+
 export const DataItem = React.forwardRef<HTMLDivElement, DataItemsProps>(function DataItem(
   { icon, title, description, className, ...props },
   ref,
 ) {
   return (
-    <Paper variant="outlined" sx={{ display: 'flex' }} className={cx(className)} ref={ref}>
-      <img src={icon} alt="" className={styles.icon(tokens.colors.shade)} />
+    <DataItemWrapper variant="outlined" className={cx(className)} ref={ref}>
+      <Box display="flex">
+        <img src={icon} alt="" className={styles.icon(tokens.colors.shade)} />
+      </Box>
       <Box sx={{ flex: '1 0 auto', p: 2, pb: 2.25 }}>
         <Typography variant="headline_ss_xxs">{title}</Typography>
         <Typography variant="body_s">{description}</Typography>
       </Box>
-    </Paper>
+    </DataItemWrapper>
   );
 });
 
@@ -32,7 +40,7 @@ const styles = {
   icon: (bgColor: string) => css`
     background-color: ${bgColor};
     width: 74px;
-    height: 79px;
+    min-height: 79px;
   `,
 };
 
