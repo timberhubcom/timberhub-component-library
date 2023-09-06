037ddf331f711b6bc8127ddc9c673bd6aba0a500
Pablo Cingolani <cingopablo@gmail.com>
2023-09-06 11:37:26 +0200

    Updated stories

--- src/theme/stories/Colors.stories.tsx
+++ src/theme/stories/Colors.stories.tsx
@@ -4,25 +4,19 @@ import React from 'react'
 import { Button, Typography } from 'src/components'
 import { tokens } from 'src/theme/tokens'
 
+import { copyTextToClipboard } from '../../utils'
+
 export default {
   title: 'Design System/Colors',
 } as Meta
 
-async function copyTextToClipboard(text: string) {
-  if ('clipboard' in navigator) {
-    return await navigator.clipboard.writeText(text)
-  } else {
-    return document.execCommand('copy', true, text)
-  }
-}
-
 const Color = ({ color = '', name = '' }: { color: string; name: string }) => (
   <div className={styles.root}>
+    <div style={{ backgroundColor: color }} />
     <div className={styles.description}>
       <Typography variant={'headline_ss_xs'}>{name}</Typography>
       <Typography variant={'body_m'}>{color}</Typography>
     </div>
-    <div style={{ backgroundColor: color }} />
     <div className={styles.actions}>
       <Button color={'grey'} size={'xs'} onClick={async () => await copyTextToClipboard(color)}>
         Copy HEX
@@ -59,7 +53,7 @@ export const Colors = ({ colors = tokens.colors, path = '' }: { colors: any; pat
 const styles = {
   root: css`
     display: grid;
-    grid-template-columns: 1fr 2fr 1fr;
+    grid-template-columns: 2fr 1fr 1fr;
     gap: 16px;
   `,
   description: css`
--- src/theme/stories/Typography.stories.tsx
+++ src/theme/stories/Typography.stories.tsx
@@ -2,10 +2,11 @@ import { css } from '@emotion/css'
 import { Divider } from '@mui/material'
 import { Meta } from '@storybook/react'
 import React from 'react'
-import { Typography } from 'src/components'
+import { Button, Typography } from 'src/components'
 import { tokens } from 'src/theme/tokens'
 
 import { CustomTypographyVariants } from '../../types'
+import { copyTextToClipboard } from '../../utils'
 
 export default {
   title: 'Design System/Typography',
@@ -18,12 +19,20 @@ export const TypographyVariants = () => (
         <div className={styles.container}>
           <Typography variant={key as CustomTypographyVariants}>The Quick Brown Fox</Typography>
           <div>
-            <Typography>{`variant: ${key}`}</Typography>
-            <Typography>{`font-family: ${value.fontFamily}`}</Typography>
-            <Typography>{`font-size: ${value.fontSize}`}</Typography>
-            <Typography>{`font-weight: ${value.fontWeight}`}</Typography>
-            <Typography>{`line-height: ${value.lineHeight}`}</Typography>
+            <Typography variant={'headline_ss_xs'}>{key}</Typography>
+            <Typography variant={'body_m'}>{`font-family: ${value.fontFamily}`}</Typography>
+            <Typography variant={'body_m'}>{`font-size: ${value.fontSize}`}</Typography>
+            <Typography variant={'body_m'}>{`font-weight: ${value.fontWeight}`}</Typography>
+            <Typography variant={'body_m'}>{`line-height: ${value.lineHeight}`}</Typography>
           </div>
+          <Button
+            color={'grey'}
+            size={'xs'}
+            onClick={async () => {
+              await copyTextToClipboard(`tokens.typography.${key}`)
+            }}>
+            Copy token
+          </Button>
         </div>
         <Divider />
       </React.Fragment>
@@ -38,6 +47,14 @@ const styles = {
   `,
   container: css`
     display: grid;
+    align-items: center;
+    grid-template-columns: 3fr 1fr 1fr;
+    gap: 16px;
+  `,
+  actions: css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
     gap: 16px;
   `,
 }
