dc300d47a7a87a26a891f748852455acd7676888
Pablo Cingolani <cingopablo@gmail.com>
2023-09-06 12:39:57 +0200

    Fixes

--- src/theme/stories/Colors.stories.tsx
+++ src/theme/stories/Colors.stories.tsx
@@ -10,31 +10,37 @@ export default {
   title: 'Design System/Colors',
 } as Meta
 
-const Color = ({ color = '', name = '' }: { color: string; name: string }) => (
-  <div className={styles.root}>
-    <div style={{ backgroundColor: color }} />
-    <div className={styles.description}>
-      <Typography variant={'headline_ss_xs'}>{name}</Typography>
-      <Typography variant={'body_m'}>{color}</Typography>
+const Color = ({ color = '', name = '' }: { color: string; name: string }) => {
+  const parsedName = React.useMemo(() => {
+    const isObject = name.includes('.')
+    if (isObject) {
+      const path = name.split('.')
+      return `${path[0]}[${path[1]}]`
+    }
+    return name
+  }, [name])
+
+  return (
+    <div className={styles.root}>
+      <div style={{ backgroundColor: color }} />
+      <div className={styles.description}>
+        <Typography variant={'headline_ss_xs'}>{parsedName}</Typography>
+        <Typography variant={'body_m'}>{color}</Typography>
+      </div>
+      <div className={styles.actions}>
+        <Button color={'grey'} size={'xs'} onClick={async () => await copyTextToClipboard(color)}>
+          Copy HEX
+        </Button>
+        <Button
+          color={'grey'}
+          size={'xs'}
+          onClick={async () => await copyTextToClipboard(`tokens.colors.${parsedName}`)}>
+          Copy token
+        </Button>
+      </div>
     </div>
-    <div className={styles.actions}>
-      <Button color={'grey'} size={'xs'} onClick={async () => await copyTextToClipboard(color)}>
-        Copy HEX
-      </Button>
-      <Button
-        color={'grey'}
-        size={'xs'}
-        onClick={async () => {
-          const path = name.split('.')
-          await copyTextToClipboard(
-            name.includes('.') ? `tokens.colors.${path[0]}[${path[1]}]` : `tokens.colors.${name}`
-          )
-        }}>
-        Copy token
-      </Button>
-    </div>
-  </div>
-)
+  )
+}
 
 export const Colors = ({ colors = tokens.colors, path = '' }: { colors: any; path: string }) => (
   <React.Fragment>
