e33c85664d913979067a2952338a3fd6bfd872b4
Oni Benjamin <onibenjo@gmail.com>
2023-09-11 13:01:41 +0100

    hotfix: react key (#92)

--- src/components/NewTable/NewTable.tsx
+++ src/components/NewTable/NewTable.tsx
@@ -61,7 +61,7 @@ export const NewTable = <TData extends object>({
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => (
                 <TableHead
-                  key={header.id}
+                  key={crypto.randomUUID()}
                   className={styles.head(header.getSize(), header.column.columnDef.enablePinning)}
                 >
                   {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
