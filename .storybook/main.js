01957e1ca3a22625fb69585cd6eed21c05721c07
Pablo Cingolani <53660145+cingopablo@users.noreply.github.com>
2023-12-05 15:43:46 +0100

    Update (#113)
    * Updates
    
    * Updates
    
    * Updates
    
    * Updates

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,26 +2,29 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-scss', '@storybook/addon-mdx-gfm'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-scss',
+    '@storybook/addon-mdx-gfm',
+  ],
   framework: {
     name: '@storybook/react-webpack5',
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: true
+    autodocs: true,
   },
   staticDirs: ['../src/assets'],
   webpackFinal: async (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      'src': path.resolve(__dirname, '../src/'),
+      src: path.resolve(__dirname, '../src/'),
     };
 
     config.module = config.module || {};
     config.module.rules = config.module.rules || [];
 
-    // This modifies the existing image rule to exclude .svg files
-    // since you want to handle those files with @svgr/webpack
     const imageRule = config.module.rules.find((rule) => rule?.['test']?.test('.svg'));
     if (imageRule) {
       imageRule['exclude'] = /\.svg$/;
@@ -35,17 +38,14 @@ module.exports = {
 
     return config;
   },
-   typescript: {
+  typescript: {
     check: false,
     checkOptions: {},
     reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
       propFilter: (prop) => {
-        return prop.parent
-          ? /@mui/.test(prop.parent.fileName) ||
-            !/node_modules/.test(prop.parent.fileName)
-          : true;
+        return prop.parent ? /@mui/.test(prop.parent.fileName) || !/node_modules/.test(prop.parent.fileName) : true;
       },
     },
   },
